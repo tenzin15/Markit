63d96e59012dbb1f0a556ba83ee2e952a2de4af8
Shauan Ferreira Leite <shauanf@gmail.com>
2017-02-05 12:01:30 -0500

    create user model

---
+++ db/models/user.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
+    username: DataTypes.STRING(32),
+    password: DataTypes.TEXT,
+    email: DataTypes.STRING(128),
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return user;
+};
