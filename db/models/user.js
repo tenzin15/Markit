9ac9fc2f4d505c1425b3e9f2ef944c16689bbcbc
Kyla Massey <sir.massey@gmail.com>
2017-02-05 15:27:25 -0500

    successfully created markit_app_dev db and user table

---
+++ db/models/user.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    email: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    userName: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
