79df34e2fe03f25481d1c344184325c545ae36cd
Tenzin Chhosphel <tenchhos@gmail.com>
2017-02-05 23:48:16 -0500

    add dabaster and creates for all entities

--- routes/folders.js
+++ routes/folders.js
@@ -7,27 +7,61 @@ var authHelpers = require('../auth/auth-helpers');
 
 // creates route to display all folders in folders database on the dom.
 router.get('/', function(req, res, next) {
-  models.Folder.findAll({ where: { user_id: req.user.dataValues.id } }).then(function(folders) {
-    res.render('folders/index', {
-      title: 'folders',
-      folders: folders,
-      user_id: req.user.dataValues.id
+  models.Folder.findAll({ where: { user_id: req.user.dataValues.id } })
+  .then(function(folders) {
+    models.Bookmarks.findAll({ where: { user_id: req.user.dataValues.id } })
+    .then(function(bookmarks) {
+      res.render('folders/index', {
+        title: 'folders',
+        folders: folders,
+        user_id: req.user.dataValues.id,
+        bookmarks: bookmarks
+      });
     });
   });
 });
 
 // creates a page that allows user to add a movie to the database
-router.get('/new', function(req, res, next) {
-  res.render('folders/new', {
-    user: req.user.dataValues
+// router.get('/new', function(req, res, next) {
+//   models.Folder.findAll({ where: { user_id: req.user.dataValues.id } }).then(function(folders) {
+//     res.render('folders/new', {
+//       user: req.user.dataValues,
+//       folders: folders
+//     });
+//   });
+// });
+
+// create a new folder
+router.post('/new', function(req, res, next) {
+  models.Folder.create({
+    title: req.body.folder_title,
+    user_id: req.body.user_id
+  }).then(function() {
+    res.redirect('/folders')
   });
 });
 
-router.post('/', function(req, res, next) {
-  // here for req.body.id, you need to get the user_id, and place it in json
-  models.Folder.create({
+// a helper function for creating a new bookmark
+router.post('/bookmarks/new', function(req, res, next) {
+  models.Folder.findAll({ where: { user_id: req.user.dataValues.id } })
+  .then(function(folders) {
+    res.render('folders/newBookmark', {
+      user: req.user.dataValues,
+      folders: folders,
+      folder_id: req.body.folder_id,
+      folder_title: req.body.folder_title
+    });
+  });
+});
+
+// create a new bookmark
+router.post('/new/bookmark', function(req, res, next) {
+  models.Bookmarks.create({
     title: req.body.title,
-    user_id: req.body.user_id
+    url: req.body.url,
+    user_id: req.body.user_id,
+    folder_id: req.body.folder_id,
+    folder_title: req.body.folder_title
   }).then(function() {
     res.redirect('/folders')
   });
@@ -44,7 +78,9 @@ router.get('/:id', function(req, res, next) {
   });
 });
 
-// GET /folders/:id/edit: this should bring the user to a form to edit the info. of the movie corresponding to the id. Don't worry about allowing the user to edit the director for now, we can't be sure that whomever is in charge of that part of the app has completed their work
+// GET /folders/:id/edit: this should bring the user to a form to edit the info. of the movie corresponding to the id.
+// Don't worry about allowing the user to edit the director for now, we can't be sure that whomever is in charge of
+// that part of the app has completed their work
 router.get('/:id/edit', function(req, res, next) {
   models.Movie.findById(req.params.id).then(function(movie) {
     res.render('foldersEdit', {
