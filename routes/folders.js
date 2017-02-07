6d629c4d38990be815fe14c16a6d318a1d9e7cd1
Tenzin Chhosphel <tenchhos@gmail.com>
2017-02-07 16:43:25 -0500

    you know how u run outta commit messages

--- routes/folders.js
+++ routes/folders.js
@@ -17,7 +17,8 @@ router.get('/', function(req, res, next) {
         user_id: req.user.dataValues.id,
         user_firstName: req.user.dataValues.firstName,
         bookmarks: bookmarks,
-        folder_id: folders[0].id
+        folder_id: folders[0].id,
+        folder_title: folders[0].title
       });
     });
   });
@@ -46,6 +47,8 @@ router.post('/bookmarks/new', function(req, res, next) {
   });
 });
 
+
+
 // only show the bookmarks that belongs to the this folder
 router.post('/bookmarks', function(req, res, next) {
     // res.send(req.body.folder_id + req.body.folder_title + req.body.folder_user_id);
@@ -70,15 +73,44 @@ router.post('/bookmarks', function(req, res, next) {
 // create a new bookmark
 router.post('/new/bookmark', function(req, res, next) {
   models.Bookmarks.create({
-    title: req.body.title,
-    url: req.body.url,
-    user_id: req.body.user_id,
+    title: 'Add Title',
+    url: req.body.enterBookmarkUrl,
+    user_id: req.user.dataValues.id,
     folder_id: req.body.folder_id,
     folder_title: req.body.folder_title
-  }).then(function() {
-    res.redirect('/folders');
+  })
+  .then(function() {
+    // res.redirect('/folders');
+    models.Folder.findAll({ where: { user_id: req.user.dataValues.id } })
+    .then(function(folders) {
+      models.Bookmarks.findAll({ where: { user_id: req.user.dataValues.id } })
+      .then(function(bookmarks) {
+        res.render('folders/index', {
+          title: 'folders',
+          folders: folders,
+          user_id: req.user.dataValues.id,
+          user_firstName: req.user.dataValues.firstName,
+          bookmarks: bookmarks,
+          folder_id: req.body.folder_id,
+          folder_title: req.body.folder_title
+        });
+      });
+    });
   });
 });
+
+// // create a new bookmark
+// router.post('/new/bookmark', function(req, res, next) {
+//   models.Bookmarks.create({
+//     title: req.body.title,
+//     url: req.body.url,
+//     user_id: req.body.user_id,
+//     folder_id: req.body.folder_id,
+//     folder_title: req.body.folder_title
+//   }).then(function() {
+//     res.redirect('/folders');
+//   });
+// });
 
 //EDIT
 router.get('/bookmark/:id/edit', function(req, res, next) {
@@ -110,45 +142,45 @@ router.delete('/bookmark/:id', function(req, res, next) {
 // });
 
 
-// creates route to folders/id# that renders movie titles and synopsis based on whichever movie id was requested
-router.get('/:id', function(req, res, next) {
-  models.Movie.findById(req.params.id).then(function(folders) {
-    res.render('moviesHome', {
-      title: folders.title,
-      folders: folders,
-      synopsis: folders.synopsis
-    });
-  });
-});
+// // creates route to folders/id# that renders movie titles and synopsis based on whichever movie id was requested
+// router.get('/:id', function(req, res, next) {
+//   models.Movie.findById(req.params.id).then(function(folders) {
+//     res.render('moviesHome', {
+//       title: folders.title,
+//       folders: folders,
+//       synopsis: folders.synopsis
+//     });
+//   });
+// });
 
-// GET /folders/:id/edit: this should bring the user to a form to edit the info. of the movie corresponding to the id.
-// Don't worry about allowing the user to edit the director for now, we can't be sure that whomever is in charge of
-// that part of the app has completed their work
-router.get('/:id/edit', function(req, res, next) {
-  models.Movie.findById(req.params.id).then(function(movie) {
-    res.render('foldersEdit', {
-      movie:movie
-    });
-  });
-});
+// // GET /folders/:id/edit: this should bring the user to a form to edit the info. of the movie corresponding to the id.
+// // Don't worry about allowing the user to edit the director for now, we can't be sure that whomever is in charge of
+// // that part of the app has completed their work
+// router.get('/:id/edit', function(req, res, next) {
+//   models.Movie.findById(req.params.id).then(function(movie) {
+//     res.render('foldersEdit', {
+//       movie:movie
+//     });
+//   });
+// });
 
-// posts edited movie info submitted (updates) don't forget method override npm install and then add to app.js
-router.put('/:id', function(req, res, next) {
-  models.Movie.update({
-    title: req.body.title,
-    synopsis: req.body.synopsis
-  }, { where: { id: req.params.id } })
-  .then(function() {
-    res.redirect('/folders/' + req.params.id);
-  });
-});
+// // posts edited movie info submitted (updates) don't forget method override npm install and then add to app.js
+// router.put('/:id', function(req, res, next) {
+//   models.Movie.update({
+//     title: req.body.title,
+//     synopsis: req.body.synopsis
+//   }, { where: { id: req.params.id } })
+//   .then(function() {
+//     res.redirect('/folders/' + req.params.id);
+//   });
+// });
 
-router.delete('/:id', function(req, res, next) {
-  models.Movie.destroy({
-    where: { id: req.params.id }
-  }).then(function(movie) {
-    res.redirect('/folders');
-  });
-});
+// router.delete('/:id', function(req, res, next) {
+//   models.Movie.destroy({
+//     where: { id: req.params.id }
+//   }).then(function(movie) {
+//     res.redirect('/folders');
+//   });
+// });
 
 module.exports = router;
